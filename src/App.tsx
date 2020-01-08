5ae0f4a8bb773045825518bed0cc2f30bf0fa4fb
Proudust <proudust@gmail.com>
2020-01-08 16:05:16 +0900

    :sparkles: add material ui test code

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,49 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  AppBar,
+  Container,
+  CssBaseline,
+  Link,
+  Paper,
+  Toolbar,
+  Typography,
+} from '@material-ui/core';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      minWidth: 350,
+    },
+    paper: {
+      margin: theme.spacing(2),
+      padding: theme.spacing(2),
+    },
+  }),
+);
 
 const App: React.FC = () => {
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.root}>
+      <CssBaseline />
+      <AppBar>
+        <Toolbar>
+          <Typography variant="h6">React App</Typography>
+        </Toolbar>
+      </AppBar>
+      <Toolbar />
+      <Container maxWidth="sm" disableGutters>
+        <Paper className={classes.paper}>
+          <Typography component="p">
+            Edit <code>src/App.tsx</code> and save to reload.
+          </Typography>
+          <Link href="https://reactjs.org" variant="body2">
+            Learn React
+          </Link>
+        </Paper>
+      </Container>
     </div>
   );
 };
